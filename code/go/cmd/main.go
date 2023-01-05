6cc43d10a1aa752c054e83fa78f4cd0393bc258f
Hailong Geng <hailong@tubi.tv>
2023-01-05 10:34:00 -0800

    try ivf reader

--- code/go/cmd/main.go
+++ code/go/cmd/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/pion/webrtc/v3/pkg/media/ivfreader"
+)
+
+func readIVF(url string) *ivfreader.IVFReader {
+	file, err := os.Open(url)
+	if err != nil {
+		log.Fatal("failed to open ivf file with error:", err)
+	}
+	reader, header, err := ivfreader.NewWith(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(header)
+	return reader
+}
 
 func main() {
-	fmt.Println("hello world")
+	reader := readIVF("../../media_sources/1.ivf")
+	for {
+		frame, header, err := reader.ParseNextFrame()
+		if frame == nil && header == nil && err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Print(header)
+	}
 }
