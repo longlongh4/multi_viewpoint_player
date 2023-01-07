c97e09c96d429db93324c8969479e7f42639d861
Hailong Geng <hailong@tubi.tv>
2023-01-07 14:50:50 -0800

    setup gh pages

--- code/go/go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/longlongh4/multi_viewpoint_playerpackager/muxer
-
-go 1.19
-
-require (
-	github.com/pion/webrtc/v3 v3.1.50
-	google.golang.org/protobuf v1.26.0
-)
--- code/web/src/assets/js/utils.js
+++
@@ -1,6 +0,0 @@
-export function joinBuffer(uint8array1, uint8array2) {
-  let mergedArray = new Uint8Array(uint8array1.length + uint8array2.length);
-  mergedArray.set(uint8array1);
-  mergedArray.set(uint8array2, uint8array1.length);
-  return mergedArray;
-}
