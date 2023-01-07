bb3262c2316c8750a631af7aa5c454ca62de2c25
hailong <longlongh4@gmail.com>
2023-01-07 09:02:50 -0800

    wrap player into class

--- code/web/src/assets/js/worker.js
+++ code/web/src/assets/js/worker.js
@@ -2,112 +2,124 @@ const utils = require("./utils");
 const Canvas2DRenderer = require("./renderer").Canvas2DRenderer;
 const parseHeader = require("./parse_header").parseHeader;
 
-let headerSize = 0;
-let mvvIndex = null;
-let decodedFrames = [];
-
-const mediaUrl = "http://localhost:8080/out.mvv";
-
-let renderer = null;
+let context = {
+  mediaUrl: "http://localhost:8080/out.mvv",
+  headerSize: 0,
+  mvvIndex: null,
+  renderer: null,
+  currentCameraPlayer: null,
+};
 let timer = {
   startedAt: -1,
   pausedAt: -1,
 };
 
+class Player {
+  constructor(cameraIndex) {
+    this.cameraIndex = cameraIndex;
+    this.camera = context.mvvIndex.cameras[cameraIndex];
+    this.fetchControl = new AbortController();
+    this.decodedFrames = [];
+    this.decoder = null;
+  }
+
+  // secondIndex: start at which second in time
+  async loadCamera(secondIndex) {
+    let decodedFrames = this.decodedFrames;
+    this.decoder = new VideoDecoder({
+      output(frame) {
+        decodedFrames.push(frame);
+        requestAnimationFrame(renderAnimationFrame);
+      },
+      error(e) {
+        console.log(e);
+      },
+    });
+    this.decoder.configure({
+      codec: "av01.0.01M.08",
+      codedWidth: 854,
+      codedHeight: 480,
+    });
+    let cameraStartOffset = (cameraIndex, secondIndex) => {
+      let camera = context.mvvIndex.cameras[cameraIndex];
+      return (
+        camera.frames[secondIndex * camera.framerate].offset +
+        context.headerSize
+      );
+    };
+
+    let byteRange =
+      this.cameraIndex === context.mvvIndex.camera_count - 1
+        ? `${cameraStartOffset(this.cameraIndex, secondIndex)}-`
+        : `${cameraStartOffset(this.cameraIndex, secondIndex)}-${
+            cameraStartOffset(this.cameraIndex + 1, 0) - 1
+          }`;
+    let response = await fetch(context.mediaUrl, {
+      headers: {
+        range: byteRange,
+      },
+      signal: this.fetchControl.signal,
+    });
+    let pendingFrames = this.camera.frames.slice(
+      secondIndex * this.camera.framerate
+    );
+    const reader = response.body.getReader();
+    let result = await reader.read();
+    let buffer = new Uint8Array();
+    while (!result.done) {
+      buffer = utils.joinBuffer(buffer, result.value);
+      buffer = this.decodeFramesFromBuffer(pendingFrames, buffer);
+      result = await reader.read();
+    }
+  }
+
+  decodeFramesFromBuffer(pendingFrames, buffer) {
+    let offset = 0;
+    while (
+      pendingFrames.length > 0 &&
+      pendingFrames[0].size + offset <= buffer.length
+    ) {
+      const frame = pendingFrames.shift();
+      if (frame.keyframe) {
+        this.decoder.flush();
+      }
+      const encodedVideoChunk = new EncodedVideoChunk({
+        type: frame.keyframe ? "key" : "delta",
+        timestamp: (frame.timestamp / 24) * 1000,
+        duration: (1 / 24) * 1000,
+        data: buffer.slice(offset, offset + frame.size),
+      });
+      this.decoder.decode(encodedVideoChunk);
+      offset += frame.size;
+    }
+    if (pendingFrames.length === 0) {
+      this.decoder.flush();
+    }
+    return buffer.slice(offset);
+  }
+}
+
 function renderAnimationFrame() {
-  console.log(performance.now());
+  let decodedFrames = context.currentCameraPlayer.decodedFrames;
   if (decodedFrames.length > 0) {
     if (timer.startedAt === -1) {
       timer.startedAt = performance.now();
     }
     if (decodedFrames[0].timestamp <= performance.now() - timer.startedAt) {
       const frame = decodedFrames.shift();
-      renderer.draw(frame);
+      context.renderer.draw(frame);
     }
     requestAnimationFrame(renderAnimationFrame);
-  }
-}
-// Set up a VideoDecoer.
-const decoder = new VideoDecoder({
-  output(frame) {
-    decodedFrames.push(frame);
-    requestAnimationFrame(renderAnimationFrame);
-  },
-  error(e) {
-    console.log(e);
-  },
-});
-
-function configDecoder() {
-  decoder.configure({
-    codec: "av01.0.01M.08",
-    codedWidth: 854,
-    codedHeight: 480,
-  });
-}
-
-function decodeFramesFromBuffer(pendingFrames, buffer) {
-  let offset = 0;
-  while (
-    pendingFrames.length > 0 &&
-    pendingFrames[0].size + offset <= buffer.length
-  ) {
-    const frame = pendingFrames.shift();
-    if (frame.keyframe) {
-      decoder.flush();
-    }
-    const encodedVideoChunk = new EncodedVideoChunk({
-      type: frame.keyframe ? "key" : "delta",
-      timestamp: (frame.timestamp / 24) * 1000,
-      duration: (1 / 24) * 1000,
-      data: buffer.slice(offset, offset + frame.size),
-    });
-    decoder.decode(encodedVideoChunk);
-    offset += frame.size;
-  }
-  if (pendingFrames.length === 0) {
-    decoder.flush();
-  }
-  return buffer.slice(offset);
-}
-
-// secondIndex: start at which second in time
-async function loadCamera(cameraIndex, secondIndex) {
-  console.log(mvvIndex);
-  let camera = mvvIndex.cameras[cameraIndex];
-  let cameraStartOffset = (cameraIndex, secondIndex) => {
-    let camera = mvvIndex.cameras[cameraIndex];
-    return camera.frames[secondIndex * camera.framerate].offset + headerSize;
-  };
-  let byteRange =
-    cameraIndex === mvvIndex.camera_count - 1
-      ? `${cameraStartOffset(cameraIndex, secondIndex)}-`
-      : `${cameraStartOffset(cameraIndex, secondIndex)}-${
-          cameraStartOffset(cameraIndex + 1, 0) - 1
-        }`;
-  let response = await fetch(mediaUrl, {
-    headers: {
-      range: byteRange,
-    },
-  });
-  let pendingFrames = camera.frames.slice(secondIndex * camera.framerate);
-  const reader = response.body.getReader();
-  let result = await reader.read();
-  let buffer = new Uint8Array();
-  while (!result.done) {
-    buffer = utils.joinBuffer(buffer, result.value);
-    buffer = decodeFramesFromBuffer(pendingFrames, buffer);
-    result = await reader.read();
   }
 }
 
 self.addEventListener("message", (message) => {
   let canvas = message.data;
-  renderer = new Canvas2DRenderer(canvas);
-  parseHeader(mediaUrl).then(([size, index]) => {
-    headerSize = size;
-    mvvIndex = index;
-    configDecoder();
-    loadCamera(0, 0);
+  context.renderer = new Canvas2DRenderer(canvas);
+  parseHeader(context.mediaUrl).then(([size, index]) => {
+    context.headerSize = size;
+    context.mvvIndex = index;
+    context.currentCameraPlayer = new Player(0);
+    context.currentCameraPlayer.loadCamera(0);
   });
 });
